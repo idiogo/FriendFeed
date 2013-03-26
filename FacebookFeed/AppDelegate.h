c0d2018a0ef73168b7b613712d658c0fae1e2b5b
Diogo Carneiro <contato@idiogo.com.br>
2013-03-25 21:19:27 -0300

    Initial Commit

---
+++ FacebookFeed/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookFeed
+//
+//  Created by Diogo Carneiro on 25/03/13.
+//  Copyright (c) 2013 Diogo Carneiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacebookFeed/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FacebookFeed
+//
+//  Created by Diogo Carneiro on 25/03/13.
+//  Copyright (c) 2013 Diogo Carneiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
